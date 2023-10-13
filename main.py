4f41f4039aa8ec81c83d64da4f2e0cfa3486aaf3
Kaoru Nishikawa <kq.nishikawa@gmail.com>
2023-10-13 14:31:26 +0900

    Fix: 単語IDが指定されなかった場合、辞書の内容が全て出力されるよう修正

--- main.py
+++ main.py
@@ -12,16 +12,17 @@ import argparse
 
 def main(args):
     with open("dictionary-data.txt", "r", encoding="utf-8") as f:
+        print_all = args.id is None
         for i, row in enumerate(f): # 行ごとにファイルを読み込む
             row_num = i + 1 # i = 0, 1, 2 ...より現在の行には i + 1 が必要
-            if row_num != int(args.id):
+            if (not print_all) and (row_num != int(args.id)):
                 continue
             print(f'{row_num}: {row}', end="") # 行数: 内容 で出力
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser() # descriptionは必要に応じて書くこと
 
-    parser.add_argument("id", help="行数の指定")
+    parser.add_argument("id", help="行数の指定", nargs="?")
 
     args = parser.parse_args()
     main(args)
